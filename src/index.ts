ad523d07fbfff46038347c78b10137a793aa77fc
GustavoNakamuraC <gustavonakamura6@gmail.com>
2024-09-21 22:08:38 -0300

    adicionando  pagina principal e arrumando data na listagem

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Usuários
+app.get('/', async function (req: Request, res: Response) {
+    let i = 0;
+
+    const posts = [
+        { title: 'Título do Post 1', description: 'Uma breve descrição sobre o conteúdo do post. Este post fala sobre...', link: '#' },
+        { title: 'Título do Post 2', description: 'Uma breve descrição sobre o conteúdo do post. Este post fala sobre...', link: '#' },
+        { title: 'Título do Post 3', description: 'Uma breve descrição sobre o conteúdo do post. Este post fala sobre...', link: '#' },
+        { title: 'Título do Post 4', description: 'Uma breve descrição sobre o conteúdo do post. Este post fala sobre...', link: '#' }
+    ];
+
+    return res.render('users/index',{
+        posts: posts
+    })
+})
+
 app.get('/users', async function (req: Request, res: Response){
-    const [rows] = await connection.query("SELECT * FROM users");
-    console.log(rows);
+    const [rows] = await connection.query(
+        "SELECT id, name, email, role, DATE_FORMAT(registerDate, '%d/%m/%Y') AS registerDate FROM users"
+    );
+    const registerDate = await connection.query("SELECT DATE_FORMAT(registerDate, '%d/%m/%Y') AS data_formatada FROM users;")
     return res.render('users/userTables',{
         users: rows
     });
@@ -34,26 +51,29 @@ app.get('/users/add', async function (req: Request, res: Response) {
 
 app.post('/users/save', async function (req: Request, res: Response) {
     const body = req.body;
-    const {password, confirmPassword} = req.body;
+    const { password, confirmPassword, role, active } = req.body;
 
     if(password != confirmPassword){
         console.log(password, confirmPassword);
         return res.render('users/register', { 
             error: 'Erro na confirmação das senhas.' 
         });
-    }else{
-        let {active} = req.body;
-        
-        if (active === 'on')
-            active = 1;
-        else
-            active = 0;
-
-        const insertQuery = 'INSERT INTO users (name, email, password, role, active) VALUES (?,?,?,?,?)';
-        await connection.query(insertQuery, [body.name, body.email, body.password, body.role, body.active])
-        res.redirect('/users');
     }
+
+    let isActive = active === 'on' ? 1 : 0;
+
+    const insertQuery = 'INSERT INTO users (name, email, password, role, active) VALUES (?,?,?,?,?)';
+    await connection.query(insertQuery, [body.name, body.email, body.password, body.role, isActive])
+    res.redirect('/users');
     
 });
+
+app.post('/users/:id/delete', async function (req: Request, res: Response) {
+    const id = req.params.id;
+    const deleteQuery = 'DELETE FROM users WHERE id = ?';
+    await connection.query(deleteQuery, [id]);
+
+    res.redirect('/users');
+})
 
 app.listen(3000, () => console.log("http://localhost:3000"));
